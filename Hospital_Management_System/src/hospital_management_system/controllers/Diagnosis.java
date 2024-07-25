d59280386fbeeb3ac4890356031f68d2d310e674
ryan1121 <aiwo8152@gmail.com>
2024-07-25 13:17:14 +0800

    Updated controller/Surgery.java

--- Hospital_Management_System/src/hospital_management_system/controllers/Diagnosis.java
+++ Hospital_Management_System/src/hospital_management_system/controllers/Diagnosis.java
@@ -5,56 +5,68 @@ import javax.swing.JOptionPane;
 
 
 public class Diagnosis {
-    public static String patientID;
-    public static String doctorID;
+    public static String PatientID;
+    public static String DoctorID;
     public static String DiagnosisID;
-    public static String dateOfDiagnosis;
-    public static String DiagnosisDesc;
-    public static String treatmentPlan;
+    public static String DateOfDiagnosis;
+    public static String DiagnosisDescription;
+    public static String treatmentPlans;
     public static javax.swing.JPanel panel;
+
+    public static javax.swing.JTextField DiagnosisID_textField;
+    public static javax.swing.JTextField DateOfDiagnosis_textField;
+    public static javax.swing.JTextArea DiagnosisDescriptionription_TextArea;
+    public static javax.swing.JTextArea treatmentPlanss_TextArea;
 
     public Diagnosis(
         javax.swing.JPanel panel,
-        javax.swing.JTextField patientID_textField,
-        javax.swing.JTextField doctorID_textField,
+        javax.swing.JTextField PatientID_textField,
+        javax.swing.JTextField DoctorID_textField,
         javax.swing.JTextField DiagnosisID_textField,
         javax.swing.JTextField DateOfDiagnosis_textField,
-        javax.swing.JTextArea DiagnosisDescription_TextArea,
-        javax.swing.JTextArea treatmentPlans_TextArea
+        javax.swing.JTextArea DiagnosisDescriptionription_TextArea,
+        javax.swing.JTextArea treatmentPlanss_TextArea
     ){
-        this.patientID = patientID_textField.getText();
-        this.doctorID = doctorID_textField.getText();
+        this.panel = panel;
+        this.PatientID = PatientID_textField.getText();
+        this.DoctorID = DoctorID_textField.getText();
         this.DiagnosisID = DiagnosisID_textField.getText();
-        this.dateOfDiagnosis = DateOfDiagnosis_textField.getText();
+        this.DateOfDiagnosis = DateOfDiagnosis_textField.getText();
         // 将日期格式调整为 YYYY-MM-DD
-        this.dateOfDiagnosis = DateUtils.formatDate(dateOfDiagnosis);
+        this.DateOfDiagnosis = DateUtils.formatDate(DateOfDiagnosis);
 
-        this.DiagnosisDesc = DiagnosisDescription_TextArea.getText();
-        this.treatmentPlan = treatmentPlans_TextArea.getText();
+        this.DiagnosisDescription = DiagnosisDescriptionription_TextArea.getText();
+        this.treatmentPlans = treatmentPlanss_TextArea.getText();
+
+
+        this.DiagnosisID_textField = DiagnosisID_textField;
+        this.DateOfDiagnosis_textField = DateOfDiagnosis_textField;
+        this.DiagnosisDescriptionription_TextArea = DiagnosisDescriptionription_TextArea;
+        this.treatmentPlanss_TextArea = treatmentPlanss_TextArea;
     }
     
 
     public static void Diagnosis_SaveButtonActionPerformed(java.awt.event.ActionEvent evt, javax.swing.JTextField DiagnosisID_textField) {//GEN-FIRST:event_Diagnosis_SaveButtonActionPerformed
         // TODO add your handling code here:
-        System.out.println("PatientID: " + patientID);
-        System.out.println("DoctorID: " + doctorID);
-        if (patientID == null || patientID.isEmpty() && (doctorID == null || doctorID.isEmpty())){
+        System.out.println("PatientID: " + PatientID);
+        System.out.println("DoctorID: " + DoctorID);
+        if (PatientID == null || PatientID.isEmpty() && (DoctorID == null || DoctorID.isEmpty())){
             JOptionPane.showMessageDialog(panel, "You MUST enter patient ID and doctor ID !!");
-        } else if (patientID == null || patientID.isEmpty()) {
+        } else if (PatientID == null || PatientID.isEmpty()) {
             System.out.println("Patient ID is blank");
             JOptionPane.showMessageDialog(panel, "You MUST enter patient ID !!");
-        } else if (doctorID == null || doctorID.isEmpty()) {
+        } else if (DoctorID == null || DoctorID.isEmpty()) {
             JOptionPane.showMessageDialog(panel, "You MUST enter doctor ID !!");
         } else {
             
             System.out.println(DiagnosisID);
-            System.out.println(dateOfDiagnosis);
-            System.out.println(DiagnosisDesc);
-            System.out.println(treatmentPlan);
+            System.out.println(DateOfDiagnosis);
+            System.out.println(DiagnosisDescription);
+            System.out.println(treatmentPlans);
 
             // 创建 MysqlConnect 对象
             MysqlConnect db = new MysqlConnect();
-            String[] values = {DiagnosisID, patientID, doctorID, DiagnosisDesc, dateOfDiagnosis, treatmentPlan};
+            String[] values = {DiagnosisID, PatientID, DoctorID, DiagnosisDescription, DateOfDiagnosis, treatmentPlans};
             try {
                 boolean saveResult = db.saveData("Diagnosis", "DiagnosisID, PatientID, DoctorID, DiagnosisDescription, DateOfDiagnosis, treatmentPlans", values);
                 if (saveResult){
@@ -65,15 +77,22 @@ public class Diagnosis {
                 setNewDiagnosisId(DiagnosisID_textField);    // reset the new diagnosis ID
             } catch (SQLException e) {
                 System.err.println("Error while saving data!");
-                e.printStackTrace();
                 JOptionPane.showMessageDialog(panel, "Error while saving data: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             }
-            
         }
 
         
 
     }//GEN-LAST:event_Diagnosis_SaveButtonActionPerformed
+
+
+    public void Diagnosis_ClearButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Diagnosis_ClearButtonActionPerformed
+        // TODO add your handling code here:
+        this.DiagnosisID_textField.setText("");
+        this.DateOfDiagnosis_textField.setText("");
+        this.DiagnosisDescriptionription_TextArea.setText("");
+        this.treatmentPlanss_TextArea.setText("");
+    }//GEN-LAST:event_Diagnosis_ClearButtonActionPerformed
 
 
     public static String setNewDiagnosisId(javax.swing.JTextField DiagnosisID_textField){
