9dcd639a99fc13e640eca3a16ec02326f12bff61
ryan1121 <aiwo8152@gmail.com>
2024-07-25 15:27:12 +0800

    Changed package hospital_management_system to package hospital_management_system.views

--- Hospital_Management_System/src/hospital_management_system/views/GUI_nurse.java
+++ Hospital_Management_System/src/hospital_management_system/views/GUI_nurse.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package hospital_management_system;
+package hospital_management_system.views;
 
 /**
  *
