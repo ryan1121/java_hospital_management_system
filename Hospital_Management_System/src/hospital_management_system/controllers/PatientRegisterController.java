404018ca2e6932af2cfbe619c7805bb096432b81
JunChen04 <jcchong041@gmail.com>
2024-08-10 12:37:55 +0800

    update login and register gui function

--- Hospital_Management_System/src/hospital_management_system/controllers/PatientRegisterController.java
+++ Hospital_Management_System/src/hospital_management_system/controllers/PatientRegisterController.java
@@ -5,6 +5,9 @@
 package hospital_management_system.controllers;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,6 +38,14 @@ public class PatientRegisterController {
     }
 
     private void submitForm() {
+        // Retrieve the password from the JPasswordField
+        char[] passwordChars = view.getPasswordTextField().getPassword();
+        char[] confirmPasswordChars = view.getjPasswordField1().getPassword();
+
+        // Convert char[] to String if needed
+        String password = new String(passwordChars);
+        String confirmPassword = new String(confirmPasswordChars);
+        
         // Get data from view
         model.setName(view.getRegisterName());
         model.setDob(view.getRegisterDOB());
@@ -47,6 +58,8 @@ public class PatientRegisterController {
         model.setEmergencyRelation(view.getRegisterEmergencyRelation());
         model.setEmergencyPhone(view.getRegisterEmergencyPhone());
         model.setGender(view.getGender());
+        model.setPassword(password);
+        model.setConfirmPassword(confirmPassword);
 
         // Validate data
         if (model.getName().isEmpty() || model.getDob().isEmpty() || model.getPhone().isEmpty() || model.getEmail().isEmpty() || model.getAddress1().isEmpty() || model.getGender() == null || model.getEmergencyName().isEmpty() || model.getEmergencyRelation().isEmpty() || model.getEmergencyPhone().isEmpty()) {
@@ -54,45 +67,62 @@ public class PatientRegisterController {
             
         } 
         else {
-            if (isValidPhone(model.getPhone())) {
-                System.out.println("Valid Phone");
-                if (isValidEmail(model.getEmail())) {
-                    System.out.println("Valid Email");
-                    if (isValidPhone(model.getEmergencyPhone())) {
-                        System.out.println("Valid Phone");
-                        // Perform further processing, such as saving the data to a database or displaying it in a message dialog
-                        String message = "Name: " + model.getName() + "\nDate of Birth: " + model.getDob() + "\nPhone: " + model.getPhone() + "\nEmail: " + model.getEmail() + "\nAddress: " + model.getAddress1() + ", " + model.getAddress2() + ", " + model.getAddress3() + "\nGender: " + model.getGender() + "\nEmergency Section" + "\nName: " + model.getEmergencyName() + "\nPhone: " + model.getEmergencyPhone() + "\nRelationship: " + model.getEmergencyRelation();
-                        JOptionPane.showMessageDialog(view, message, "Registration Information", JOptionPane.INFORMATION_MESSAGE);
-                        String newID = db.generateNewId("Patients", "P");
-
-                        String tableName = "Patients";
-                        String columns = "patient_id, patient_DOB, patient_gender, patient_phone, patient_name, patient_password, patient_email, patient_address, patient_address_line2, patient_address_line3, patient_emergency_name, patient_emergency_phone, patient_emergency_relationship";
-                        String[] values = {newID, model.getDob(), model.getGender(), model.getPhone(), model.getName(), "password"+newID , model.getEmail(), model.getAddress1(), model.getAddress2(), model.getAddress3(), model.getEmergencyName(), model.getEmergencyPhone(), model.getEmergencyRelation()};
-
-                        try {
-                            boolean success = db.saveData(tableName, columns, values);
-                            if (success) {
-                                System.out.println("Data inserted successfully!");
-                            } else {
-                                System.out.println("Data insertion failed.");
+            if (password.equals(confirmPassword)) {
+                if (isValidPhone(model.getPhone())) {
+                    System.out.println("Valid Phone");
+                    if (isValidEmail(model.getEmail())) {
+                        System.out.println("Valid Email");
+                        if (isValidPhone(model.getEmergencyPhone())) {
+                            System.out.println("Valid Phone");
+                            // Perform further processing, such as saving the data to a database or displaying it in a message dialog
+                            String message = "Name: " + model.getName() + "\nDate of Birth: " + model.getDob() + "\nPhone: " + model.getPhone() + "\nEmail: " + model.getEmail() + "\nAddress: " + model.getAddress1() + ", " + model.getAddress2() + ", " + model.getAddress3() + "\nGender: " + model.getGender() + "\nEmergency Section" + "\nName: " + model.getEmergencyName() + "\nPhone: " + model.getEmergencyPhone() + "\nRelationship: " + model.getEmergencyRelation();
+                            JOptionPane.showMessageDialog(view, message, "Registration Information", JOptionPane.INFORMATION_MESSAGE);
+                            
+                            SimpleDateFormat originalFormat = new SimpleDateFormat("MM/dd/yyyy"); // Adjust if original format is different
+                            SimpleDateFormat targetFormat = new SimpleDateFormat("yyyy/MM/dd");
+                            Date date = null;
+                            try {
+                                date = originalFormat.parse(model.getDob());
+                            } catch (ParseException e) {
+                                JOptionPane.showMessageDialog(view, "Invalid Date of Birth format.", "Error", JOptionPane.ERROR_MESSAGE);
+                                e.printStackTrace();
+                                return; // Exit if date parsing fails
                             }
-                        } catch (SQLException e) {
-                            System.err.println("Error occurred while inserting data.");
-                            e.printStackTrace();
-                        }
+                            String formattedDob = targetFormat.format(date);
+                            
+                            String newID = db.generateNewId("Patients", "P");
 
-                        view.dispose();
-                        GUI_patient Patient_GUI = new GUI_patient(newID);
-                        Patient_GUI.setVisible(true);
+                            String tableName = "Patients";
+                            String columns = "patient_id, patient_DOB, patient_gender, patient_phone, patient_name, patient_password, patient_email, patient_address, patient_address_line2, patient_address_line3, patient_emergency_name, patient_emergency_phone, patient_emergency_relationship";
+                            String[] values = {newID, formattedDob, model.getGender(), model.getPhone(), model.getName(), password , model.getEmail(), model.getAddress1(), model.getAddress2(), model.getAddress3(), model.getEmergencyName(), model.getEmergencyPhone(), model.getEmergencyRelation()};
+
+                            try {
+                                boolean success = db.saveData(tableName, columns, values);
+                                if (success) {
+                                    System.out.println("Data inserted successfully!");
+                                } else {
+                                    System.out.println("Data insertion failed.");
+                                }
+                            } catch (SQLException e) {
+                                System.err.println("Error occurred while inserting data.");
+                                e.printStackTrace();
+                            }
+
+                            view.dispose();
+                            GUI_patient Patient_GUI = new GUI_patient(newID);
+                            Patient_GUI.setVisible(true);
+                        }else{
+                            JOptionPane.showMessageDialog(null, "Invalid Emergency Phone", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
                     }else{
-                        JOptionPane.showMessageDialog(null, "Invalid Emergency Phone", "Error", JOptionPane.ERROR_MESSAGE);
+                        JOptionPane.showMessageDialog(null, "Invalid Email", "Error", JOptionPane.ERROR_MESSAGE);
                     }
                 }else{
-                    JOptionPane.showMessageDialog(null, "Invalid Email", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "Invalid Phone Number", "Error", JOptionPane.ERROR_MESSAGE);
                 }
-            }else{
-                JOptionPane.showMessageDialog(null, "Invalid Phone Number", "Error", JOptionPane.ERROR_MESSAGE);
-            }
+            } else{
+                JOptionPane.showMessageDialog(null, "Passwords do not match");
+            }   
             
             
         }
@@ -111,6 +141,8 @@ public class PatientRegisterController {
         view.setRegisterEmergencyPhone("");
         view.setGenderMale(false);
         view.setGenderFemale(false);
+        view.setPassword("");
+        view.setConfirmPassword("");
     }
 
     private boolean isValidEmail(String email) {
